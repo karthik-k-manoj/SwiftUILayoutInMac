b51ec7b5b047c5d7e654a59a81c55d61320f30f1
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-02-06 11:42:48 +0530

    finish indrastructure

--- SwiftUILayoutInMac/ContentView.swift
+++ SwiftUILayoutInMac/ContentView.swift
@@ -63,16 +63,29 @@ extension View_ {
     }
 }
 
-protocol Shape_ {
+protocol Shape_: View_ {
     func path(in rect: CGRect) -> CGPath
+}
+
+extension Shape_ {
+    var body: some View_ {
+        ShapeView(shape: self)
+    }
+}
+
+extension NSColor: View_ {
+    var body: some View_ {
+        ShapeView(shape: Rectangle_(), color: self)
+    }
 }
 
 struct ShapeView<S: Shape_>: BuiltinView, View_ {
     var shape: S
+    var color: NSColor = .red
     
     func render(context: RenderingContext, size: ProposedSize) {
         context.saveGState()
-        context.setFillColor(NSColor.red.cgColor)
+        context.setFillColor(color.cgColor)
         context.addPath(shape.path(in: CGRect(origin: .zero, size: size)))
         context.fillPath()
         context.restoreGState()
@@ -85,10 +98,16 @@ struct Rectangle_: Shape_ {
     }
 }
 
-let sample = ShapeView(shape: Rectangle_())
+struct Ellipse_ : Shape_ {
+    func path(in rect: CGRect) -> CGPath {
+        CGPath(ellipseIn: rect, transform: nil)
+    }
+}
+
+let sample = NSColor.blue 
 
 func render<V: View_>(view: V) -> Data {
-    let size = CGSize(width: 400, height: 600)
+    let size = CGSize(width: 600, height: 400 )
     return CGContext.pdf(size: size) { context in
         view._render(context: context, size: size)
     }
